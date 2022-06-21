be67c94fbbeeb4d5de9a46562508fbec35d0e37c
cgarcia0 <cgarcia0@stanford.edu>
2022-06-20 23:56:55 -0700

    Updates

---
+++ precache-manifest.d6bb49a8c2ec45c1db3fc375305e855a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9bb9e3077489732d7ef5",
+    "url": "/TicTacToe/static/css/main.801734c6.chunk.css"
+  },
+  {
+    "revision": "9bb9e3077489732d7ef5",
+    "url": "/TicTacToe/static/js/main.9bb9e307.chunk.js"
+  },
+  {
+    "revision": "fd0cfea648d4e488b62d",
+    "url": "/TicTacToe/static/js/1.fd0cfea6.chunk.js"
+  },
+  {
+    "revision": "1a4430c086131fba29bb",
+    "url": "/TicTacToe/static/js/runtime~main.1a4430c0.js"
+  },
+  {
+    "revision": "22f6bdfb70346a52980854e60281c923",
+    "url": "/TicTacToe/index.html"
+  }
+];
